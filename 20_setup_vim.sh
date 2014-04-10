76e9dd9da25d9b82a30572f7d6bd381a994487d0
Jan Groth <asiandub@gmx.de>
2014-04-10 22:46:03 +1000

    Added configuration for gvim

--- 20_setup_vim.sh
+++ 20_setup_vim.sh
@@ -6,10 +6,12 @@ echo `basename $0`
 
 rm -rf ~/.vim
 rm -f ~/.vimrc
+rm -f ~/.gvimrc
 
 cp -r files/vim ~
 mv ~/vim ~/.vim
 
 ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.vim/.gvimrc ~/.gvimrc
 
 mkdir -p ~/.vim/.vimswap
