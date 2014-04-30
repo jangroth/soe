5b5ef253a1610ca903068f698ff7fc21789e4313
Jan Groth <asiandub@gmx.de>
2014-04-30 21:59:08 +1000

    added ~/.profile to bash setup

--- 10_setup_bash.sh
+++ 10_setup_bash.sh
@@ -7,9 +7,11 @@ echo `basename $0`
 rm -f ~/.bashrc
 rm -f ~/.bash_aliases
 rm -f ~/.bash_profile
+rm -f ~/.profile
 
 cp -r files/bash/.bashrc ~/
 cp -r files/bash/.bash_aliases ~/
 cp -r files/bash/.bash_profile ~/
+cp -r files/bash/.profile ~/
 
 echo "updated bash - if you want to see the changes in this shell, run source ~/.bashrc"
