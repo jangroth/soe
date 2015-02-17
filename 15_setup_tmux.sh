db92dc04aacaa660dbe073748488228e0dc73338
Jan Groth <asiandub@gmx.de>
2015-02-17 21:40:25 +1100

    Added tmux configuration

---
+++ 15_setup_tmux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+source ./config.sh
+
+print_info_text "tmux"
+
+[ -f $USER_HOME/.tmux.conf ] && rm -f $USER_HOME/.tmux.conf
+
+cp -r files/tmux/.tmux.conf $USER_HOME/
+
+echo "updated tmux"
