086542bd605ef87ce598fa7ceda0d6e0d65c62ae
Jan Groth <jan.groth.de@gmail.com>
2018-04-02 12:47:25 +1000

    Add awesume alias

---
+++ 50_setup_aws.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+source ./config.sh
+
+print_info_text "awsume"
+
+[ -f $USER_HOME ] && rm -f $USER_HOME/.gitconfig
+[ -f $USER_HOME ] && rm -f $USER_HOME/.gitcompletion
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o files/git/gitcompletion
+
+cp files/git/gitconfig $USER_HOME/.gitconfig
+cp files/git/gitcompletion $USER_HOME/.gitcompletion
