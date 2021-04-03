c87cd934197181d50b0da06459dea4f22eac4342
Jan Groth <jan.groth.de@gmail.com>
2021-04-03 16:22:25 +1100

    fix

--- scripts/40_setup_git.sh
+++ scripts/40_setup_git.sh
@@ -10,7 +10,7 @@ confirm_binaries "git"
 cp -rf "${DOT_ROOT}/files/git/gitconfig" $HOME/.gitconfig
 
 # update remote dependencies
-if [ "${DOT_REINSTALL}" = "true" ] || [ ! -f "$HOME/.vim/autoload/pathogen.vim" ]; then
+if [ "${DOT_REINSTALL}" = "true" ] || [ ! -f "$HOME/.zsh/git-completion.bash" ]; then
     mkdir -p $HOME/.zsh
     curl -LSso $HOME/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
     curl -LSso $HOME/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
